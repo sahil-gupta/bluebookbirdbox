f6efc293a76cbb26b6cdb1b57afc96da608f4e81
Sahil Gupta <sahil@spase.io>
2019-01-13 19:21:03 -0500

    store classes in localstor

--- src/js/vueClasses.js
+++ src/js/vueClasses.js
@@ -3,13 +3,8 @@ const logg = console.log;
 const client = algoliasearch('KRM1S7DDHX', 'f109d17791c3399d3d83ed3848d230d4');
 const index = client.initIndex('spring_classes');
 
-const ical = require('ical-generator');
 const moment = require('moment');
-
-const SECONDSINDAY = 86400;
-const TODAY = moment().startOf('day');
-const RANGEFUTURE = TODAY.diff(moment('2020-05-20'), 'days');   // negative number
-const RANGEPAST = TODAY.diff(moment('2019-01-01'), 'days');     // positive number
+const ical = require('ical-generator');
 
 function localstorage(key, value) {
     if (value === undefined) {
@@ -18,34 +13,6 @@ function localstorage(key, value) {
     }
     window.localStorage.setItem(key, JSON.stringify(value));
 }
-
-// const cal = ical({
-//     domain: 'sebbo.net',
-//     prodId: { company: 'superman-industries.com', product: 'ical-generator' },
-//     name: 'My Testfeed',
-//     timezone: 'Europe/Berlin'
-// });
-// // You can also set values like this…
-// cal.domain('sebbo.net');
-// // … or get values
-// logg(cal.domain()); // --> "sebbo.net"
-// // create a new event
-// logg(moment())
-// logg('this')
-// const event = cal.createEvent({
-//     start: moment(),
-//     end: moment().add(1, 'hour'),
-//     timestamp: moment(),
-//     summary: 'My Event',
-//     organizer: 'Sebastian Pekarek <mail@example.com>'
-// });
-// // like above, you can also set/change values like this…
-// event.summary('My Super Mega Awesome Event');
-// // get the iCal string
-// logg(cal.toString());
-// logg('hereee')
-
-
 
 
 function run() {
@@ -74,6 +41,7 @@ function run() {
             chooseresult: function (theresult) {
                 this.chosens.push(theresult);
                 this.chosens = _.uniqBy(this.chosens, 'objectID');
+                localstorage('bbbb_classes', this.chosens);
             },
             choosefirstresult: function () {
                 if (this.searchresults.length === 0) {
@@ -82,16 +50,49 @@ function run() {
                 var theresult = this.searchresults[0];
                 this.chosens.push(theresult);
                 this.chosens = _.uniqBy(this.chosens, 'objectID');
+                localstorage('bbbb_classes', this.chosens);
             },
-            removechosen: function(chosen) {
+            removechosen: function (chosen) {
                 _.remove(this.chosens, function (anything) {
                     return anything.objectID === chosen.objectID;
                 });
                 this.chosens.push('');
                 this.chosens.pop(-1);
+                localstorage('bbbb_classes', this.chosens);
             },
-            exportics: function() {
-                logg('asdjflksdjflkasjlkjk');
+            exportics: function () {
+
+
+                const TODAY = moment().startOf('day');
+                const RANGEFUTURE = TODAY.diff(moment('2020-05-20'), 'days');   // negative number
+                const RANGEPAST = TODAY.diff(moment('2019-01-01'), 'days');     // positive number
+
+                // const cal = ical({
+                //     domain: 'sebbo.net',
+                //     prodId: { company: 'superman-industries.com', product: 'ical-generator' },
+                //     name: 'My Testfeed',
+                //     timezone: 'Europe/Berlin'
+                // });
+                // // You can also set values like this…
+                // cal.domain('sebbo.net');
+                // // … or get values
+                // logg(cal.domain()); // --> "sebbo.net"
+                // // create a new event
+                // logg(moment())
+                // logg('this')
+                // const event = cal.createEvent({
+                //     start: moment(),
+                //     end: moment().add(1, 'hour'),
+                //     timestamp: moment(),
+                //     summary: 'My Event',
+                //     organizer: 'Sebastian Pekarek <mail@example.com>'
+                // });
+                // // like above, you can also set/change values like this…
+                // event.summary('My Super Mega Awesome Event');
+                // // get the iCal string
+                // logg(cal.toString());
+                // logg('hereee')
+
             }
         },
         watch: {
@@ -105,6 +106,7 @@ function run() {
             }
         },
         mounted: function () {
+            this.chosens = localstorage('bbbb_classes');
             this.debounceddosearch = _.debounce(this.dosearch, 500);
 
         }
